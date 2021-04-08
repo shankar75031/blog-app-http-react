9bac59419eb76b24118d3ada5704da9358bc4878
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 22:38:45 +0530

    create post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 import './NewPost.css';
@@ -7,6 +8,18 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const post = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post("https://jsonplaceholder.typicode.com/posts/", post)
+        .then(res => {
+            console.log(res)
+        }).catch(err => console.log(err))
     }
 
     render () {
@@ -19,10 +32,10 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Prabhashankar">Prabhashankar</option>
+                    <option value="Sangeetha">Sangeetha</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
