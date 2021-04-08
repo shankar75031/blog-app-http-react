6d6a208e544e7ff9c5ebafbb416c4ed8e29adcf3
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 21:55:26 +0530

    fetch post on clicking a post and display it in fullpost

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,29 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state ={
+        loadedPost: null
+    }
+
+    componentDidUpdate() {
+       if(this.props.id){
+        axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id).then(res => {
+            this.setState({loadedPost: res.data})
+        }).catch(err => console.log(err))
+    
+       }}
+
     render () {
         let post = <p style={{textAlign: "center"}} >Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.id){post = <p style={{textAlign: "center"}} >Loading...</p>;}
+        if(this.state.loadedPost){
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.content}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
