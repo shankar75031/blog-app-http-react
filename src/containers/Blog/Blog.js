b6e9b4dbb7e6819cf200abe34c06a43ffbf8c8f6
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 21:42:20 +0530

    display full post based on selected post id

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -25,9 +26,13 @@ class Blog extends Component {
         .catch(err => console.error(err))
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} author={post.author} key={post.id}/>
+            return <Post title={post.title} author={post.author} key={post.id} clicked={() => this.postSelectedHandler(post.id)}/>
         })
         return (
             <div>
@@ -35,7 +40,7 @@ class Blog extends Component {
                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
