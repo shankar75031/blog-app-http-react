4e13d1c56a0feeed87805042ebfbe8779b78ff6f
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 22:48:36 +0530

    err handling in blog component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,53 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  };
 
-    state = {
-        posts: [],
-        selectedPostId: null
-    }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          post.author = "Snowman";
+          return post;
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((err) => {
+        this.setState({ error: true });
+      });
+  }
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            const posts = response.data.slice(0,4)
-            const updatedPosts = posts.map(post => {
-                post.author = "Snowman"
-                return post
-            })
-            this.setState({posts: updatedPosts})
-        })
-        .catch(err => console.error(err))
-    }
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
-
-    render () {
-        const posts = this.state.posts.map(post => {
-            return <Post title={post.title} author={post.author} key={post.id} clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
         return (
-            <div>
-                <section className="Posts">
-                   {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            title={post.title}
+            author={post.author}
+            key={post.id}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
+      });
     }
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
